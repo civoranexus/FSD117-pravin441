3a0fa0e470a8c1a244d51795f2fe8fbe2a481d68
pravin441 <ps3400486@gmial.com>
2026-01-16 21:38:57 +0530

    update app.js file

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
   return 
-  (
-    <div>
+  (<div>
       <h1>Counter App</h1>
       <p>Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>
